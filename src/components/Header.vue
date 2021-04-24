77f37c64c719c57211296fac706f1b291cc83875
Bishal Neupane <neupanebishal07@gmail.com>
2021-04-24 07:42:40 +0545

    Button and Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>This is the header component {{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: { type: String, required: false, default: 'No title given' },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: aquamarine;
+  font-family: cursive;
+}
+</style>
